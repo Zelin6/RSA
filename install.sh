244dada03d345ae206e258824b7217fe0b1b357e
Andy <131517198+Zelin6@users.noreply.github.com>
2024-10-20 16:32:44 +0800

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,6 @@
+sudo apt-get install libssl-dev
+sleep 3
+gcc -o decrypt_rsa decrypt_rsa.c -lssl -lcrypto 
+gcc -o rsa_example rsa_example.c -lssl -lcrypto
+sudo cp ./rsa_example /usr/bin
+sudo cp ./decrypt_rsa /usr/bin
